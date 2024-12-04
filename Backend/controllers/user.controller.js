4ee54e74f59ab2fd252c88774e60040aad9a3161
subrat8268 <subrato8268@gmail.com>
2024-12-04 15:30:42 +0530

    captain register

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -11,6 +11,12 @@ module.exports.registerUser = async (req, res, next) => {
 
   const { fullname, email, password } = req.body;
 
+  const isUserAlreadyRegistered = await userModel.findOne({ email });
+
+  if (isUserAlreadyRegistered) {
+    return res.status(400).json({ message: "User already registered" });
+  }
+
   const hashPassword = await userModel.hashPassword(password);
 
   const user = await userService.createUser({
